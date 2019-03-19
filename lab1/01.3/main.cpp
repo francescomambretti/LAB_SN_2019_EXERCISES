b5e9b8b3a537d4fdee95933864deb7484e401534
francescomambretti <francesco.mambretti@unimi.it>
2019-03-19 23:07:41 +0100

    Before Lesson 2

--- lab1/01.3/main.cpp
+++ lab1/01.3/main.cpp
@@ -8,7 +8,7 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 *****************************************************************
 *****************************************************************/
 
-//Modified by Francesco Mambretti, 08/03/2019
+//Modified by Francesco Mambretti, 09/03/2019
 
 #include <iostream>
 #include <fstream>
@@ -46,54 +46,55 @@ int main (int argc, char *argv[]){
 
 //Buffon's needle
 double L = 1.0;
-double d = 2.1;
-int N_hit=0;
+double d = 1.2;
+int N_hit;
 double x_c, x_t, y_t;
 double dist2=0,dist=0;
 double radius=L/2.;
 ofstream out;
 out.open("PI.dat");
-double PI, PI2;
+double PI=0, PI2=0;
+int blk_size=500, nblk=0;
+int tot_steps = 100000;
 
-vector<int> N_throws = {10,100,1000,2500,5000,10000,25000,50000,100000,250000,500000,1000000,2500000,5000000};
+nblk=tot_steps/blk_size;
 
-for (int j=0; j<N_throws.size(); ++j){
-	PI=0.;
-	PI2=0.;
-	for (int t=0; t < 10; t++){
-		N_hit=0;
-		for (int i=0; i < N_throws[j]; i++){
-			x_c=rnd.Rannyu(0,d);
-			//now I have to extract needle's direction
-			//I generate random pairs of (x_t, y_t); these coordinates are accepted (and indicate a direction) as long as they are inside a circle centred in (x_c,0) whose radius is L/2
-			do {
-				x_t=rnd.Rannyu(-1,1);
-				y_t=rnd.Rannyu(-1,1);	
-				dist2=(x_t)*(x_t)+(y_t)*(y_t);
-				dist=sqrt(dist2);
-			} while( dist > 1); //stop when the extracted point falls inside the circle
+for (int i=0; i < nblk; ++i){
+	N_hit=0;
+	for (int t=0; t < blk_size; ++t){
+		x_c=rnd.Rannyu(0,d);
+		//now I have to extract needle's direction
+		//I generate random pairs of (x_t, y_t); these coordinates are accepted (and indicate a direction) as long as they are inside a circle centred in (x_c,0) whose radius is L/2
+		do {
+			x_t=rnd.Rannyu(-1,1);
+			y_t=rnd.Rannyu(-1,1);	
+			dist2=(x_t)*(x_t)+(y_t)*(y_t);
+			dist=sqrt(dist2);
+		} while( dist > 1); //stop when the extracted point falls inside the circle
 	
-			if (x_t>0){
-				if (x_c+x_t*radius/dist>d || x_c-x_t*radius/dist<0)
-					N_hit++;
-			}
+		if (x_t>0){
+			if (x_c+x_t*radius/dist>d || x_c-x_t*radius/dist<0)
+				N_hit++;
+		}
 	
-			else{
-				if (x_c+x_t*radius/dist<0 || x_c-x_t*radius/dist>d)
-					N_hit++;
-			}	
+		else{
+			if (x_c+x_t*radius/dist<0 || x_c-x_t*radius/dist>d)
+				N_hit++;
+		}	
 
-		}//end i-for
-		PI+=2*L/d*(double(N_throws[j])/double(N_hit));
-		PI2+=(2*L/d*(double(N_throws[j])/double(N_hit)))*(2*L/d*(double(N_throws[j])/double(N_hit)));
-		//out << N_throws[j] << '\t' << 2*L/d*(double(N_throws[j])/double(N_hit)) << endl;
-		//cout << "With " <<  N_throws[j] << " throws, PI is equal to = " << 2*L/d*(double(N_throws[j])/double(N_hit)) << endl;
 	}//end t-for
-		out << N_throws[j] << ' ' << PI/10.0 << ' ' << sqrt(PI2/10.0-PI/10.0*PI/10.0) << endl;
-}//end j-for
-   	rnd.SaveSeed();
+	PI+=2*L/d*(double(blk_size)/double(N_hit));
+	PI2+=(2*L/d*(double(blk_size)/double(N_hit)))*(2*L/d*(double(blk_size)/double(N_hit)));
 
-out;
+	if (i>0)
+		out << i << ' ' << PI/double(i+1) << ' ' << sqrt( PI2/double(i+1)-(PI/double(i+1))*(PI/double(i+1)) )/sqrt(i) << endl;
+	else
+		out << i << ' ' << PI/double(i+1) << ' ' << 0. << endl;
+}//end i-for
+   	
+	rnd.SaveSeed();
+
+out.close();
 
 return 0;
 }
--- lab1/01.3/plot.py
+++ lab1/01.3/plot.py
@@ -4,14 +4,14 @@ import numpy as np
 
 #01.3
 plt.suptitle('Buffon\'s needle: PI estimation')
+plt.title('L=1, d=1.2, blk_size=500, tot_steps=1000')
 x,f,error = np.loadtxt("PI.dat", usecols=(0,1,2), delimiter=' ', unpack='true')
 
-plt.xscale('log')
-plt.errorbar(x,f,error,fmt='o',markersize='4',capsize=4,color='C2')
-plt.xlabel('$N_throws$')
-plt.ylabel('$PI \: estimation$')
+plt.errorbar(x,f,error,fmt='o',markersize='2',capsize=3,color='C2',ecolor='C2',elinewidth=1,errorevery=2)
+plt.xlabel('$N_{blocks}$')
+plt.ylabel('PI estimation')
 
-plt.hlines(3.1415,0,5000000)
+plt.hlines(3.14159,0,200)
 
 plt.savefig('01.3.png')
 plt.show()
